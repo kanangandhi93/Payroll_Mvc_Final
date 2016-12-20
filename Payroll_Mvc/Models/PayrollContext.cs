eff0fb196d335061198f5010bb3256fbe699d70f
Kanan Gandhi <Kanan Gandhi@DESKTOP-PPUAUTB>
2016-12-20 06:59:38 +0530

    edit

---
+++ Payroll_Mvc/Models/PayrollContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Payroll_Mvc.Models
+{
+    public class PayrollContext : DbContext
+    {
+        //public DbSet<country>
+    }
+}
---
+++ Payroll_Mvc/Models/country.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Payroll_Mvc.Models
+{
+    public class country
+    {
+        public Int64 Id { get; set; }
+        public String CountryName{get;set;}
+    }
+}
