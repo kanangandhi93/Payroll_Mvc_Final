5ff11a8c3938f96cc9fd106de28cb98925aca066
Kanan Gandhi <Kanan Gandhi@DESKTOP-PPUAUTB>
2016-12-23 22:16:39 +0530

    23_12_2016_Night

--- Payroll_Mvc/Controllers/countryController.cs
+++ Payroll_Mvc/Controllers/countryController.cs
@@ -89,5 +89,7 @@ namespace Payroll_Mvc.Controllers
             }
             return View();
         }
+
+        
     }
 }
--- Payroll_Mvc/Controllers/stateController.cs
+++ Payroll_Mvc/Controllers/stateController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using BusinessLayer;
+using System.Configuration;
+
 namespace Payroll_Mvc.Controllers
 {
     public class stateController : Controller
@@ -38,6 +40,76 @@ namespace Payroll_Mvc.Controllers
                 return RedirectToAction("Index");
             }
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(Int64 id)
+        {
+
+            stateBusinessLayer StateBusinessLayer = new stateBusinessLayer();
+            state State = StateBusinessLayer.States.Single(x => x.Id == id);
+            ConfigurationManager.AppSettings["EditId"] = id.ToString();
+
+            CountryBusinessLayer countryBusinessLayer = new CountryBusinessLayer();
+            List<SelectListItem> selectlistitems = new List<SelectListItem>();
+            foreach (Country country in countryBusinessLayer.countries)
+            {
+                SelectListItem selectListItem = new SelectListItem
+                {
+                    Text = country.CountryName,
+                    Value = country.Id.ToString(),
+                    Selected = country.Id == State.CountryId ? true : false
+                };
+                selectlistitems.Add(selectListItem);
+            }
+            ViewBag.countries = selectlistitems;
+            return View(State);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(FormCollection formCollection)
+        {
+            state State = new state();
+            State.State = formCollection["State"];
+            State.CountryId = Convert.ToInt64(formCollection["countries"]);
+            State.Id = Convert.ToInt64(ConfigurationManager.AppSettings["EditId"].ToString());
+
+            stateBusinessLayer StateBusinessLayer = new stateBusinessLayer();
+            int rv = StateBusinessLayer.UpdateState(State);
+            if (rv != 0 && rv != -1)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Delete(Int64 id)
+        {
+            stateBusinessLayer StateBusinessLayer = new stateBusinessLayer();
+            state State = StateBusinessLayer.States.Single(x => x.Id == id);
+            ConfigurationManager.AppSettings["EditId"] = id.ToString();
+            CountryBusinessLayer countryBusinessLayer = new CountryBusinessLayer();
+            Country country = countryBusinessLayer.countries.Single(x => x.Id == State.CountryId);
+            State.CountryName = country.CountryName;
+            return View(State);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(FormCollection formCollection)
+        {
+            stateBusinessLayer StateBusinessLayer = new stateBusinessLayer();
+            state State = new state();
+            State.State = formCollection["State"];
+            State.CountryId = Convert.ToInt64(formCollection["CountryId"]);
+            State.Id = Convert.ToInt64(ConfigurationManager.AppSettings["EditId"].ToString());
+            int rv = StateBusinessLayer.DeleteState(State);
+            if (rv != 0 && rv != -1)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+
         }
     }
 }
