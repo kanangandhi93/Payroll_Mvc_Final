7759f6165a6671a1676a8066db9c44d60154b26c
Kanan Gandhi <Kanan Gandhi@DESKTOP-PPUAUTB>
2016-12-26 22:31:40 +0530

    26_11_2016_10_31_PM

--- BusinessLayer/CompanyDetailBusinessLayer.cs
+++ BusinessLayer/CompanyDetailBusinessLayer.cs
@@ -33,25 +33,23 @@ namespace BusinessLayer
                     companydetails Companydetails = new companydetails();
                     Companydetails.AddressLine1 = reader["AddressLine1"].ToString();
                     Companydetails.AddressLine2 = reader["AddressLine2"].ToString();
-                    Companydetails.AddressLine3 = reader["AddressLine2"].ToString();
+                    Companydetails.AddressLine3 = reader["AddressLine3"].ToString();
                     Companydetails.city = reader["city"].ToString();
-                    Companydetails.CompanyLogo = reader["CompanyLogo "].ToString();
+                    Companydetails.CompanyLogo = reader["CompanyLogo"].ToString();
                     Companydetails.CompanyName = reader["CompanyName"].ToString();
                     Companydetails.ContactNo1 = Convert.ToInt64(reader["ContactNo1"].ToString());
                     Companydetails.ContactNo2 = Convert.ToInt64(reader["ContactNo2"].ToString());
                     Companydetails.Country = reader["Country"].ToString();
-                    Companydetails.CUser =Convert.ToInt64( reader["CUser"].ToString());
                     Companydetails.EmailId = reader["EmailId"].ToString();
                     Companydetails.FaxNo = Convert.ToInt64(reader["FaxNo"].ToString());
-                    Companydetails.FoundedYear = reader["FoundedYear "].ToString();
+                    Companydetails.FoundedYear = reader["FoundedYear"].ToString();
                     Companydetails.Founder1 = reader["Founder1"].ToString();
                     Companydetails.Founder2 = reader["Founder2"].ToString();
                     Companydetails.Founder3 = reader["Founder3"].ToString();
                     Companydetails.Founder4 = reader["Founder4"].ToString();
                     Companydetails.Founder5 = reader["Founder5"].ToString();
-                    Companydetails.potalcode = reader["potalcode"].ToString();
+                    Companydetails.potalcode = reader["PostalCode"].ToString();
                     Companydetails.state = reader["state"].ToString();
-                    Companydetails.UUser = Convert.ToInt64(reader["UUser"].ToString());
                     Companydetails.Id = Convert.ToInt64(reader["Id"].ToString());
                     companydetails.Add(Companydetails);
                 }
@@ -68,7 +66,27 @@ namespace BusinessLayer
             cmd.CommandText = "SP_tbl_Company";
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.Parameters.Add(new SqlParameter("@Mode", "Insert"));
-        
+            cmd.Parameters.Add(new SqlParameter("@AddressLine1", companydetails.AddressLine1));
+            cmd.Parameters.Add(new SqlParameter("@AddressLine2", companydetails.AddressLine2));
+            cmd.Parameters.Add(new SqlParameter("@AddressLine3", companydetails.AddressLine3));
+            cmd.Parameters.Add(new SqlParameter("@city", companydetails.city));
+            cmd.Parameters.Add(new SqlParameter("@CompanyLogo", companydetails.CompanyLogo));
+            cmd.Parameters.Add(new SqlParameter("@CompanyName", companydetails.CompanyName));
+            cmd.Parameters.Add(new SqlParameter("@ContactNo1", companydetails.ContactNo1));
+            cmd.Parameters.Add(new SqlParameter("@ContactNo2", companydetails.ContactNo2));
+            cmd.Parameters.Add(new SqlParameter("@Country", companydetails.Country));
+            cmd.Parameters.Add(new SqlParameter("@CUser", companydetails.CUser));
+            cmd.Parameters.Add(new SqlParameter("@EmailId", companydetails.EmailId));
+            cmd.Parameters.Add(new SqlParameter("@FaxNo", companydetails.FaxNo));
+            cmd.Parameters.Add(new SqlParameter("@FoundedYear", companydetails.FoundedYear));
+            cmd.Parameters.Add(new SqlParameter("@Founder1", companydetails.Founder1));
+            cmd.Parameters.Add(new SqlParameter("@Founder2", companydetails.Founder2));
+            cmd.Parameters.Add(new SqlParameter("@Founder3", companydetails.Founder3));
+            cmd.Parameters.Add(new SqlParameter("@Founder4", companydetails.Founder4));
+            cmd.Parameters.Add(new SqlParameter("@Founder5", companydetails.Founder5));
+            cmd.Parameters.Add(new SqlParameter("@PostalCode", companydetails.potalcode));
+            cmd.Parameters.Add(new SqlParameter("@state", companydetails.state));
+
             cmd.Connection = sqlConnection1;
 
             sqlConnection1.Open();
@@ -90,8 +108,27 @@ namespace BusinessLayer
             cmd.CommandText = "SP_tbl_Company";
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.Parameters.Add(new SqlParameter("@Mode", "Update"));
-            cmd.Parameters.Add(new SqlParameter("@companydetailsName", companydetails.companydetailsName));
-            cmd.Parameters.Add(new SqlParameter("@Id", Id));
+            cmd.Parameters.Add(new SqlParameter("@AddressLine1", companydetails.AddressLine1));
+            cmd.Parameters.Add(new SqlParameter("@AddressLine2", companydetails.AddressLine2));
+            cmd.Parameters.Add(new SqlParameter("@AddressLine3", companydetails.AddressLine3));
+            cmd.Parameters.Add(new SqlParameter("@city", companydetails.city));
+            cmd.Parameters.Add(new SqlParameter("@CompanyLogo", companydetails.CompanyLogo));
+            cmd.Parameters.Add(new SqlParameter("@CompanyName", companydetails.CompanyName));
+            cmd.Parameters.Add(new SqlParameter("@ContactNo1", companydetails.ContactNo1));
+            cmd.Parameters.Add(new SqlParameter("@ContactNo2", companydetails.ContactNo2));
+            cmd.Parameters.Add(new SqlParameter("@Country", companydetails.Country));
+            cmd.Parameters.Add(new SqlParameter("@UUser", companydetails.UUser));
+            cmd.Parameters.Add(new SqlParameter("@EmailId", companydetails.EmailId));
+            cmd.Parameters.Add(new SqlParameter("@FaxNo", companydetails.FaxNo));
+            cmd.Parameters.Add(new SqlParameter("@FoundedYear", companydetails.FoundedYear));
+            cmd.Parameters.Add(new SqlParameter("@Founder1", companydetails.Founder1));
+            cmd.Parameters.Add(new SqlParameter("@Founder2", companydetails.Founder2));
+            cmd.Parameters.Add(new SqlParameter("@Founder3", companydetails.Founder3));
+            cmd.Parameters.Add(new SqlParameter("@Founder4", companydetails.Founder4));
+            cmd.Parameters.Add(new SqlParameter("@Founder5", companydetails.Founder5));
+            cmd.Parameters.Add(new SqlParameter("@potalcode", companydetails.potalcode));
+            cmd.Parameters.Add(new SqlParameter("@state", companydetails.state));
+            cmd.Parameters.Add(new SqlParameter("@Id", companydetails.Id));
             cmd.Connection = sqlConnection1;
 
             sqlConnection1.Open();
@@ -127,34 +164,6 @@ namespace BusinessLayer
             return rv;
         }
 
-        public IEnumerable<companydetails> countriesById
-        {
-            get
-            {
-                List<companydetails> Contries = new List<companydetails>();
-                SqlConnection sqlConnection1 = new SqlConnection(ConfigurationManager.ConnectionStrings["PayrollContext"].ConnectionString);
-                SqlCommand cmd = new SqlCommand();
-                SqlDataReader reader;
-
-                cmd.CommandText = "SP_tbl_Company";
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.Add(new SqlParameter("@Mode", "Select"));
-                cmd.Parameters.Add(new SqlParameter("@Id", Id));
-                cmd.Connection = sqlConnection1;
-
-                sqlConnection1.Open();
-
-                reader = cmd.ExecuteReader();
-                while (reader.Read())
-                {
-                    companydetails companydetails = new companydetails();
-                    companydetails.companydetailsName = reader["companydetailsName"].ToString();
-                    companydetails.Id = Convert.ToInt64(reader["Id"].ToString());
-                    Contries.Add(companydetails);
-                }
-                sqlConnection1.Close();
-                return Contries;
-            }
-        }
+      
     }
 }
---
+++ Payroll_Mvc/Controllers/companydetailController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer;
+using System.IO;
+
+namespace Payroll_Mvc.Controllers
+{
+    public class companydetailController : Controller
+    {
+        // GET: companydetail
+        public ActionResult Index()
+        {
+            CompanyDetailBusinessLayer companyDetailBusinessLayer = new CompanyDetailBusinessLayer();
+            List<companydetails> Companydetails = companyDetailBusinessLayer.Companydetails.ToList();
+            return View(Companydetails);
+        }
+
+        // GET: companydetail/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: companydetail/Create
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        // POST: companydetail/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection, HttpPostedFileBase postedFile)
+        {
+            //try
+            //{
+            // TODO: Add insert logic here
+            if (postedFile != null)
+            {
+                string path = Server.MapPath("~/Company_Logo/");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                var fileName = Path.GetFileName(postedFile.FileName);
+                var paths = Path.Combine(Server.MapPath("~/Company_Logo/"), fileName);
+                postedFile.SaveAs(paths);
+               // postedFile.SaveAs(Server.MapPath("~/Company_Logo/" + Path.GetFileName(postedFile.FileName)));
+                ViewBag.Message = "File uploaded successfully.";
+                CompanyDetailBusinessLayer companyDetailBusinessLayer = new CompanyDetailBusinessLayer();
+                companydetails Companydetails = new companydetails();
+                Companydetails.AddressLine1 = collection["AddressLine1"];
+                Companydetails.AddressLine2 = collection["AddressLine2"];
+                Companydetails.AddressLine3 = collection["AddressLine3"];
+                Companydetails.city = collection["city"];
+                Companydetails.CompanyLogo = "~/Company_Logo/"+ fileName;
+                Companydetails.CompanyName = collection["CompanyName"];
+                Companydetails.ContactNo1 = Convert.ToInt64(collection["ContactNo1"]);
+                Companydetails.ContactNo2 = Convert.ToInt64(collection["ContactNo2"]);
+                Companydetails.Country = collection["Country"];
+                Companydetails.CUser = 1;
+                Companydetails.EmailId = collection["EmailId"];
+                Companydetails.FaxNo = Convert.ToInt64(collection["FaxNo"]);
+                Companydetails.FoundedYear = collection["FoundedYear"];
+                Companydetails.Founder1 = collection["Founder1"];
+                Companydetails.Founder2 = collection["Founder2"];
+                Companydetails.Founder3 = collection["Founder3"];
+                Companydetails.Founder4 = collection["Founder4"];
+                Companydetails.Founder5 = collection["Founder5"];
+                Companydetails.potalcode = collection["potalcode"];
+                Companydetails.state = collection["state"];
+                Companydetails.CUser = Convert.ToInt64(collection["CUser"]);
+                int rv = companyDetailBusinessLayer.Addcompanydetails(Companydetails);
+                if (rv != -1 && rv != 0)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View();
+            //return RedirectToAction("Index");
+            //}
+            //catch
+            //{
+            //    return View();
+            //}
+        }
+
+        // GET: companydetail/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: companydetail/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: companydetail/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: companydetail/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
